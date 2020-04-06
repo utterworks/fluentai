c3ee488e6b6afe650ce23f19835aae0e62fb8cca
Ubuntu <ubuntu@ip-172-31-46-77.eu-west-1.compute.internal>
2020-04-06 16:24:35 +0000

    added download scripts

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # fluentai
-Installation scripts for the Fluent AI Platform
+
+The package allows the registered users of the Fluent AI platform to download the resources to setup the Fluent platform infrastructure on their AWS Accounts.
+
+Infrastructue resources created:
+
+1. Networking resources (VPC, Subnets and security groups) In case you want to use the ones already setup, skip this step
+2. Fluent Load balancer
+3. Fluent API 
+4. Fluent Conversational Builder ECS resources
+5. Fluent UI WebApp
+6. Fluent Builder WebApp
+
+## Getting Started
+
+Please follow README.md guide for installation of each of the above components.
+
+### Prerequisites
+
+Please contact utterworks to register your account to use Fluent Platform.
+
+### Installing
+
+Download the latest packages using the following commands:
+
+```bash
+bash scripts/download_packages.sh
+```
+
+Follow the installation sections for README.md for each of the sections.
+
+
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning.
+
+## Authors
+
+- **Kaushal Trivedi** - utterworks plc
+
---
+++ scripts/download_packages.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DEPLOYMENT_BUCKET="fluent-deployment"
+
+echo "... initial clean" 
+rm -rf packages
+
+echo "... setting up" 
+mkdir packages
+
+cd packages
+
+echo "2. Downloading Fluent Load balancer"
+aws s3 cp s3://$DEPLOYMENT_BUCKET/releases/fluent_load_balancer/fluent_load_balancer-latest.zip .
+unzip fluent_load_balancer-latest.zip -d .
+mv fluent_load_balancer 2_fluent_load_balancer
+rm fluent_load_balancer-latest.zip
+
+echo "3. Downloading Fluent API"
+aws s3 cp s3://$DEPLOYMENT_BUCKET/releases/fluent_app/fluent-x-app-latest.zip .
+unzip fluent-x-app-latest.zip -d .
+mv build 3_fluent_x_app
+rm fluent-x-app-latest.zip
+
+echo "4. Downloading Fluent Builder IAC"
+aws s3 cp s3://$DEPLOYMENT_BUCKET/releases/fluent_builder_iac/fluent_builder_iac-latest.zip .
+unzip fluent_builder_iac-latest.zip -d .
+mv fluent_builder_iac 4_fluent_builder_iac
+rm fluent_builder_iac-latest.zip
+
+echo "5. Downloading Fluent UI WebApp"
+aws s3 cp s3://$DEPLOYMENT_BUCKET/releases/fluent_ui/fluent_ui-latest.zip .
+unzip fluent_ui-latest.zip -d .
+mv fluent_ui 5_fluent_ui
+rm fluent_ui-latest.zip
+
+echo "6. Downloading Fluent Builder WebApp"
+aws s3 cp s3://$DEPLOYMENT_BUCKET/releases/fluent_builder_app/fluent_builder_app-latest.zip .
+unzip fluent_builder_app-latest.zip -d .
+mv fluent_builder_app 6_fluent_builder_app
+rm fluent_builder_app-latest.zip
